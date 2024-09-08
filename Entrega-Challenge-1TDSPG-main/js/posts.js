5bd58048e35fd50d9a7dfb0ef7fe91f2484c0f8f
Gustavo Lima Reis <129910259+gustavoreislima13@users.noreply.github.com>
2024-09-08 18:05:27 -0300

    Add files via upload

---
+++ Entrega-Challenge-1TDSPG-main/js/posts.js
@@ -0,0 +1,97 @@
+var posts = [
+  {
+    "imagem": "imagens/bem-estar1110.png",
+    "titulo": "Plataforma de Bem-Estar",
+    "autor": "Porto Seguro",
+    "imagem_post": "imagens/bem-estar1110.png",
+    "texto": "Uma plataforma que combina recursos de gerenciamento financeiro pessoal com programas de bem-estar físico e mental. A IA pode oferecer insights sobre como as decisões financeiras afetam o bem-estar geral e fornecer recomendações personalizadas para alcançar objetivos financeiros e de saúde."
+  },
+  {
+    "imagem": "imagens/risco.png",
+    "titulo": "Previsão de Riscos.",  
+    "autor": "Porto Seguro",  
+    "imagem_post": "imagens/risco1.png",
+    "texto": "Um sistema de IA que analisa os dados dos clientes e do ambiente para prever riscos específicos e oferecer recomendações personalizadas para minimizá-los. Isso poderia incluir conselhos sobre segurança doméstica, sugestões de estilo de vida mais saudável para reduzir os riscos de saúde, entre outros.",
+
+  },
+  {
+    "imagem": "imagens/fraudes.png",
+    "titulo": "Prevenção de Fraudes",
+    "autor": "Porto Seguro",
+    "imagem_post": "imagens/fraudes1.png",
+    "texto": "Um sistema de IA que analisa padrões de comportamento e transações para detectar atividades suspeitas e prevenir fraudes antes que ocorram. Ele pode alertar os clientes sobre atividades incomuns em suas contas e tomar medidas proativas para proteger suas informações.",
+    
+  },
+  {
+    "imagem": "imagens/assistencia.png",
+    "titulo": "Assistência Jurídica",
+    "autor": "Porto Seguro",
+    "imagem_post": "imagens/assistencia1.png",
+    "texto": "Um serviço de consultoria jurídica virtual alimentado por IA, que oferece orientação jurídica rápida e acessível aos segurados em questões relacionadas a seguros, contratos e outros assuntos legais.",
+    
+  },
+  {
+    "imagem": "imagens/medico.png",
+    "titulo": "Assistência Médica",
+    "autor": "Porto Seguro",
+    "imagem_post": "imagens/medico1.png",
+    "texto": "Uma plataforma de telemedicina alimentada por IA que permite aos segurados consultar médicos e receber orientação médica sem sair de casa. Isso pode incluir diagnósticos preliminares, prescrições de medicamentos e acompanhamento de condições crônicas.",
+    
+  },
+  {
+    "imagem": "imagens/assistente-virtual.png",
+    "titulo": "Assistente Virtual ",
+    "autor": "Porto Seguro",
+    "imagem_post": "imagens/assistente-virtual1.png",
+    "texto": "Um assistente virtual alimentado por IA que guia os clientes durante a apresentação de sinistros, fornecendo instruções claras e coletando todas as informações necessárias de forma eficiente. Ele pode até mesmo processar sinistros simples automaticamente, reduzindo os tempos de espera e simplificando o processo para os segurados.",
+    
+  },
+  {
+    "imagem": "imagens/bem-estar.png",
+    "titulo": "Financeiro",
+    "autor": "Porto Seguro",
+    "imagem_post": "imagens/bem-estar1110.png",
+    "texto": "Uma plataforma de IA que analisa os hábitos de gastos e as metas financeiras dos clientes para oferecer conselhos personalizados sobre como economizar dinheiro e investir de forma mais eficaz. Isso pode incluir sugestões de planos de seguro mais adequados às necessidades do cliente.",
+    
+  },
+  {
+    "imagem": "imagens/risco.png",
+    "titulo": "Risco",
+    "autor": "Porto Seguro",
+    "imagem_post": "imagens/risco1.png",
+    "texto": "Um sistema de IA que analisa os dados dos clientes e do ambiente para prever riscos específicos e oferecer recomendações personalizadas para minimizá-los. Isso poderia incluir conselhos sobre segurança doméstica, sugestões de estilo de vida mais saudável para reduzir os riscos de saúde, entre outros",
+    
+  },
+  {
+    "imagem": "imagens/fraudes.png",
+    "titulo": "Fraude",
+    "autor": "Porto Seguro",
+    "imagem_post": "imagens/fraude1.png",
+    "texto": "Um sistema de IA que analisa padrões de comportamento e transações para detectar atividades suspeitas e prevenir fraudes antes que ocorram. Ele pode alertar os clientes sobre atividades incomuns em suas contas e tomar medidas proativas para proteger suas informações.",
+    
+  },
+  {
+    "imagem": "imagens/assistencia.png",
+    "titulo": "Assistencia",
+    "autor": "Porto Seguro",
+    "imagem_post": "imagens/assistencia1.ong.png",
+    "texto": "Um serviço de consultoria jurídica virtual alimentado por IA, que oferece orientação jurídica rápida e acessível aos segurados em questões relacionadas a seguros, contratos e outros assuntos legais.",
+    
+  },
+  {
+    "imagem": "imagens/medico.png",
+    "titulo": "Medico",
+    "autor": "Porto Seguro",
+    "imagem_post": "imagens/medico1.png",
+    "texto": "Uma plataforma de telemedicina alimentada por IA que permite aos segurados consultar médicos e receber orientação médica sem sair de casa. Isso pode incluir diagnósticos preliminares, prescrições de medicamentos e acompanhamento de condições crônicas.",
+    
+  },
+  {
+    "imagem": "imagens/assistente-virtual.png",
+    "titulo": "Assistente Virtual",
+    "autor": "Porto Seguro",
+    "imagem_post": "imagens/assistente-virtual1.png",
+    "texto": "Um assistente virtual alimentado por IA que guia os clientes durante a apresentação de sinistros, fornecendo instruções claras e coletando todas as informações necessárias de forma eficiente. Ele pode até mesmo processar sinistros simples automaticamente, reduzindo os tempos de espera e simplificando o processo para os segurados.",
+    
+  }
+];
